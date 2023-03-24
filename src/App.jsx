7a3afeea4453cefa16727666b71cf233ecde0b7a
thandinhjs <dinhcongthan03@gmail.com>
2023-03-24 23:45:11 +0700

    useEfffect hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import Content from "./Content";
 
+
+
+//1. useEfffect (callback);
+//2. useEfffect (callback,[]);
+//3. useEfffect (callback,[deps]);
+
+//-------------------
+
+// 1. callback luôn được gọi sau khi conponent mounted
 function App() {
 
   const [show,setShow]=useState(false);
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,38 @@
+
+// //1. useEfffect (callback);
+// -- gọi callback mỗi khi conponent re-reander
+//2. useEfffect (callback,[]);
+//3. useEfffect (callback,[deps]);
+
+import { useEffect, useState } from "react";
+
+//-------------------
+
+// 1. callback luôn được gọi sau khi conponent mounted
 function Content() {
+    const [title, setTitle] = useState('')
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => response.json())
+            .then((posts) => {
+                setPosts(posts)
+            })
+    }, [])
     return (
-        <h1>HI ae nhé</h1>
+        <div>
+            <input type="text"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <ul>
+                {posts.map((post) => {
+                    return <li key={post.id}>{post.title}</li>
+                })}
+            </ul>
+        </div>
+
     )
 }
 export default Content;
